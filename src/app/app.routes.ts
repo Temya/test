e109894c388ef794a1ebe131d7ea03f674dc5ec2
Росса Артём Дмитриевич <rossa@sc.softclub.by>
2023-11-03 14:31:47 +0300

    Add main page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from "@angular/router";
 
 export const appRoutes: Routes = [
     {path: "", pathMatch: "full", redirectTo: "autorization"},
-    {path: "autorization", loadComponent: () => import("./authorization/authorization.component").then((i) => i.AuthorizationComponent)}
+    {path: "autorization", loadComponent: () => import("./authorization/authorization.component").then((i) => i.AuthorizationComponent)},
+    {path: "main", loadComponent: () => import("./main-page/main-page.component").then((i) => i.MainPageComponent)}
 ];
